dea446b432d8bac588b07b31e2d98e44c166cd88
Roger Qiu <roger.qiu@polyhack.io>
2022-05-11 18:37:40 +1000

    Removed useless comment for pkgs.nix

--- pkgs.nix
+++ pkgs.nix
@@ -1,7 +1,3 @@
-# import (
-#   let rev = "53caacaf56640d04180775aee016d2f16d6f083c"; in
-#   fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz"
-# )
 import (
   let rev = "a5774e76bb8c3145eac524be62375c937143b80c"; in
   builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz"
